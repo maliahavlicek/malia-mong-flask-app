b167d8456d331ac74357631c48e045706d2d5fdf
maliahavlicek <malia.havlicek@gmail.com>
2020-01-05 19:50:47 -0700

    add, edit, delete categories

--- app.py
+++ app.py
@@ -52,6 +52,56 @@ def update_task(task_id):
     return redirect(url_for('get_tasks'))
 
 
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    """ FUTURE: COULD update to have is_complete being set then update get_tasks to only show tasks that are not completed"""
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    # convert task_id to bson object so we can use the task
+    the_cat = mongo.db.categories.find_one({'_id': ObjectId(category_id)})
+    return render_template('edit_category.html', category=the_cat)
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    cat = mongo.db.categories
+    cat.update({'_id': ObjectId(category_id)},
+                 {
+                     'category_name': request.form.get('category_name')
+                 })
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('add_categories.html')
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    categories.insert_one(request.form.to_dict())
+    # some validation in the future here and html
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    """ FUTURE: COULD update to have is_complete being set then update get_tasks to only show tasks that are not completed"""
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", "5000")),
